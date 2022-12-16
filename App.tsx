6ac846c0d90bcaa1d4901a168d6a8cd10ebc5b98
admin <admin@Akash-MacBook-Pro.local>
2022-12-16 12:03:32 +0530

    added sliding animation to modal

--- App.tsx
+++ App.tsx
@@ -19,6 +19,7 @@ export default function App() {
         onBackDropPress={function (): void {
           setVisible(false);
         }}
+        contentContainerStyle={{marginTop: 'auto',width: '100%',}}
       />
     </SafeAreaView>
   );
